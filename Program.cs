0e68591cf182b37cfc2bc89485802e492d507aad
Martin Patino <martin@sibipro.com>
2020-08-17 10:14:52 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chsharp_learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
